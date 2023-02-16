82419b456429f205fed2fd6e0280821e5e92d9f3
Ivan <ivan_reyesl5@outlook.com>
2023-02-15 20:48:19 -0600

    Formulario

--- Ejercicio2/longitud.php
+++ Ejercicio2/longitud.php
@@ -7,6 +7,55 @@
     <title>Document</title>
 </head>
 <body>
+
+
+    <div class="contenedor">
+
+        <div class="longitud">
+
+            <h1>Conversor de Longitudes</h1>
+
+            
+
+                <form method="POST">
+
+                    <label for="cantidad">Ingrese la cantidad:</label>
+
+                    <input type="number" class="cantidad" placeholder="Cantidad">
+
+            
+
+                    <select name="Longitud" id="longitud">
+
+                        <option selected>Selecciona la Longitud.</option>
+                        <option value="hay miguel">Metro</option>
+                        <option value="2">Pulgadas</option>
+                        <option value="3">Yardas</option>
+                        <option value="4">Pies</option>
+
+                    </select><br>
+
+                    <input type="submit" value="Enviar" name="Enviar"><br>
+                    
+                </form>
+            
+            <?php
+
+                if(isset($_POST['Longitud'])){
+                    $valor = $_POST['Longitud'];
+                    ?>
+
+                        <h1><?php echo $valor ?></h1>
+
+                    <?php
+                }
+            ?>
+            
+        </div>
+
+    </div>
+
+
     
 </body>
 </html>
