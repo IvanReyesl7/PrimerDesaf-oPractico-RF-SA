2e30b3cc4335e8a5d0aae8fbf7d433bba5717046
Ivan <ivan_reyesl5@outlook.com>
2023-02-17 21:46:08 -0600

    Ejercicio 2 Terminado

--- Ejercicio2/longitud.php
+++ Ejercicio2/longitud.php
@@ -4,56 +4,154 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500&display=swap" rel="stylesheet"> 
+    <title>Conversor de medidas</title>
 </head>
 <body>
 
 
-    <div class="contenedor">
+    <section>
 
-        <div class="longitud">
+    <div class="encabezado">
 
-            <h1>Conversor de Longitudes</h1>
+        <h1>Conversor De Longitudes</h1>
 
-            
+    </div>
+    <div class="formulario">
 
-                <form method="POST">
+        <form method="POST">
 
-                    <label for="cantidad">Ingrese la cantidad:</label>
+                    <label for="">Ingrese la cantidad:</label>
 
-                    <input type="number" class="cantidad" placeholder="Cantidad">
+                    <input name="Cantidad" type="number" class="cantidad" placeholder="Cantidad">
 
-            
+                    <label for="">Longitud:</label>
 
-                    <select name="Longitud" id="longitud">
+                    <select name="Longitud" class="cantidad" id="longitud">
 
-                        <option selected>Selecciona la Longitud.</option>
-                        <option value="hay miguel">Metro</option>
+                        <option selected value="">Selecciona la Longitud.</option>
+                        <option value="1">Metro</option>
                         <option value="2">Pulgadas</option>
                         <option value="3">Yardas</option>
                         <option value="4">Pies</option>
 
                     </select><br>
 
-                    <input type="submit" value="Enviar" name="Enviar"><br>
-                    
-                </form>
-            
-            <?php
+                    <input class="submit" type="submit" value="Enviar" name="Enviar"><br>
 
-                if(isset($_POST['Longitud'])){
-                    $valor = $_POST['Longitud'];
+                    <div class="tabla">
+                        
+                    <?php
+
+                            if(isset($_POST['Longitud']) && strlen($_POST['Cantidad'])){
+
+                                $valor = $_POST['Cantidad'];
+                                $longitud = $_POST['Longitud'];
+
+
+                                echo "<table border=1>";
+                                echo "<thead>";
+                                echo "<tr>";
+                                echo "<th>Longitud</th>";
+                                echo "<th>Cantidad</th>";
+                                echo "</th>";
+                                echo "</thead>";
+
+                                
+                                if( $longitud == "1"){
+                                    
+                                    echo "<tr>";
+                                    echo "<td>Metros</td>";
+                                    echo "<td>$valor</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Pulgadas</td>";
+                                    echo "<td>".$valor * 39.37."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Yardas</td>";
+                                    echo "<td>".$valor * 1.094."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Pies</td>";
+                                    echo "<td>".$valor * 3.281."</td>";
+                                    echo "</tr>";
+                                }elseif($longitud == "2"){
+                                    echo "<tr>";
+                                    echo "<td>Metros</td>";
+                                    echo "<td>".$valor * 39.37."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Pulgadas</td>";
+                                    echo "<td>".$valor."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Yardas</td>";
+                                    echo "<td>".$valor/36 ."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Pies</td>";
+                                    echo "<td>".$valor/12 ."</td>";
+                                    echo "</tr>";
+                                }
+                                elseif($longitud == "3"){
+                                    echo "<tr>";
+                                    echo "<td>Metros</td>";
+                                    echo "<td>".$valor / 1.094 ."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Pulgadas</td>";
+                                    echo "<td>".$valor * 36 ."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Yardas</td>";
+                                    echo "<td>".$valor."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Pies</td>";
+                                    echo "<td>".$valor * 3 ."</td>";
+                                    echo "</tr>";
+                                }
+                                elseif($longitud == "4"){
+                                    echo "<tr>";
+                                    echo "<td>Metros</td>";
+                                    echo "<td>".$valor / 3.28 ."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Pulgadas</td>";
+                                    echo "<td>".$valor * 12 ."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Yardas</td>";
+                                    echo "<td>".$valor * 0.333."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td>Pies</td>";
+                                    echo "<td>".$valor."</td>";
+                                    echo "</tr>";
+                                }
+
+                                
+                                echo "</table>";
+
+                                
+                            }
                     ?>
 
-                        <h1><?php echo $valor ?></h1>
-
-                    <?php
-                }
-            ?>
-            
-        </div>
+                    </div>
+        </form>
 
     </div>
+
+    </section>
+    
+            
+    
+            
+
 
 
     
